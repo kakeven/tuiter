48cb8d0101ec546a948981e45207c81b8aff0d9f
Kauã Keven <kevsnsilba@gmail.com>
2025-02-22 10:28:17 -0300

    inicio da timeline

--- twitter.c
+++ twitter.c
@@ -8,9 +8,19 @@ typedef struct{
     char nome[15];
 }cadastro;
 
+typedef struct{
+    cadastro login[15];
+    char frase[100];
+}post;
+
+
+
 void limpar_buffer(){
     while (getchar() != '\n');
 }
+
+
+
 
 int main(){
    //arquivo
@@ -28,13 +38,30 @@ int main(){
 
 
     cadastro *cadastros=malloc(sizeof(cadastro)*tamanho);
-    int resposta_interna,resposta_menu;
+    int resposta_interna,resposta_menu_cadastro,resposta_programa;
     int i=0;
     
 
-    
-    
+    //programa
+    while(1){
+    printf("+----------------------------+\n");
+    printf("| 1 - Timeline               |\n");
+    printf("| 2 - Menu de cadastro       |\n");
+    printf("| 3 - estatistica de hashtag |\n");
+    printf("+----------------------------+\n");
+    printf("Selecione uma opcao: "); 
+    scanf("%d",&resposta_programa);
 
+    switch (resposta_programa){
+    case 1: resposta_programa=1;
+        ;;;
+        break;
+    
+    
+        
+    
+    case 2: resposta_programa=2;
+    //menu de cadastro
     while(1){
     //menu
     printf("+---------------------+\n");
@@ -43,13 +70,13 @@ int main(){
     printf("| 3 - sair            |\n");
     printf("+---------------------+\n");
     printf("Digite a opcao desejada: ");
-    scanf("%d",&resposta_menu);
+    scanf("%d",&resposta_menu_cadastro);
     limpar_buffer();
     
     
-    switch (resposta_menu){
+    switch (resposta_menu_cadastro){
     
-    case 1: resposta_menu=1;
+    case 1: resposta_menu_cadastro=1;
         
         while(1){
             arquivos_dados=fopen("Dados Usuarios.txt","a+");
@@ -110,54 +137,54 @@ int main(){
     break; 
         
 
-    case 2: resposta_menu=2;
+    case 2: resposta_menu_cadastro=2;
         
-    while(1){
-    
-        //abertura do arquivo tamanho
-        arquivo_tamanho=fopen("tamanho.txt","r");
-        //abertura do arquivo de dados
-        arquivos_dados=fopen("Dados Usuarios.txt","r");
+        while(1){
         
-        //pega o numero que tem no arquivo de tamanho
-        fscanf(arquivo_tamanho,"%d",&tamanho);
+            //abertura do arquivo tamanho
+            arquivo_tamanho=fopen("tamanho.txt","r");
+            //abertura do arquivo de dados
+            arquivos_dados=fopen("Dados Usuarios.txt","r");
+            
+            //pega o numero que tem no arquivo de tamanho
+            fscanf(arquivo_tamanho,"%d",&tamanho);
+            
+            
+            //ler os dados do arquivo
+            int i=0;
+        while ( i < tamanho && fscanf(arquivos_dados, "%s %s %s\n", cadastros[i].nome, cadastros[i].login, cadastros[i].senha) == 3) {
+            i++;
+        }
+            
+        //fecha o arquivo de dados
+        fclose(arquivos_dados);
         
         
-        //ler os dados do arquivo
-        int i=0;
-    while ( i < tamanho && fscanf(arquivos_dados, "%s %s %s\n", cadastros[i].nome, cadastros[i].login, cadastros[i].senha) == 3) {
-        i++;
+        
+        printf("Tamanho: %d",tamanho);
+        
+        printf("\n=== Usuarios Cadastrados ===\n");
+        
+        //mostra os dados lidos
+        for (int j = 0; j < tamanho-1; j++) {
+            printf("Usuario %d:\n", j );
+            printf("Nome: %s\n", cadastros[j].nome);
+            printf("Login: %s\n", cadastros[j].login);
+            printf("Senha: %s\n\n", cadastros[j].senha);
+        }
+        fclose(arquivo_tamanho);
+        break;
     }
-        
-    //fecha o arquivo de dados
-    fclose(arquivos_dados);
-    
-    
-    
-    printf("Tamanho: %d",tamanho);
-    
-    printf("\n=== Usuarios Cadastrados ===\n");
-    
-    //mostra os dados lidos
-    for (int j = 0; j < tamanho-1; j++) {
-        printf("Usuario %d:\n", j );
-        printf("Nome: %s\n", cadastros[j].nome);
-        printf("Login: %s\n", cadastros[j].login);
-        printf("Senha: %s\n\n", cadastros[j].senha);
-    }
-    fclose(arquivo_tamanho);
-    break;
-}
     break;
     
         
         
-        case 3: resposta_menu=3;
+    case 3: resposta_menu_cadastro=3;
             exit(1);
 
-            break;
-        default:
-            break;
+        break;
+    default:
+        break;
         }
         
     
@@ -165,8 +192,16 @@ int main(){
     
 
     }
-
     free(cadastros);
+    break;
+    
+default:
+        break;
+
+    }
+    }
+
+
 }
     
 
